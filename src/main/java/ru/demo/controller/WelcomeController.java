7c5976e42592243f49d325753608d6825829c24a
Баринов Сергей Владимирович <SVBarinov.SBT@sberbank.ru>
2017-09-07 10:43:17 +0300

    books

---
+++ src/main/java/ru/demo/controller/WelcomeController.java
@@ -0,0 +1,27 @@
+package ru.demo.controller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+
+	// inject via application.properties
+	@Value("${welcome.message:test}")
+	private String message = "Hello World";
+
+	@RequestMapping("/welkome")
+	public String welcome(Map<String, Object> model) {
+		model.put("message", this.message);
+		return "welcome";
+	}
+	@RequestMapping("/healthcheck")
+	public String healthCheck(Map<String,Object> model) {
+		model.put("text","OK");
+		return "plain";
+	}
+
+}
