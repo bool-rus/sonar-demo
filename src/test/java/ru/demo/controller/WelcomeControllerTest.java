f9983e079c9ffeac7cf3e3f5bf46a26d33ba3d80
Баринов Сергей Владимирович <SVBarinov.SBT@sberbank.ru>
2017-09-07 17:24:48 +0300

    fix err

--- src/test/java/ru/demo/controller/WelcomeControllerTest.java
+++ src/test/java/ru/demo/controller/WelcomeControllerTest.java
@@ -13,7 +13,7 @@ public class WelcomeControllerTest {
     public void welcome() throws Exception {
         WelcomeController welcomeController = new WelcomeController();
         Map<String,Object> model = new HashMap<>();
-        String result = welcomeController.welcome(model);
+        String result = welcomeController.Welcome(model);
         assertEquals("welcome",result);
         assertEquals("Hello World", model.get("message"));
     }
